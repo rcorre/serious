51c50a52ab2aa3c1fbcf8efdbce89fc40a297ca1
Ryan <ryan@rcorre.net>
2015-04-05 14:26:03 -0400

    Rework deserialization logic.
    Rely on 3 data functions: getEntry, asPrimitive, and asRange.

--- src/serious/deserialize.d
+++ src/serious/deserialize.d
@@ -1,42 +1,79 @@
 module serious.deserialize;
 
 import std.array     : array;
+import std.range     : isInputRange, ElementType;
 import std.string    : format;
-import std.traits    : isAggregateType, isArray, isSomeString;
+import std.traits    : isAggregateType, isArray, isSomeString, hasMember;
 import std.algorithm : map;
 
-T deserialize(T, Data, Conv)(Data data, Conv conv) {
+T deserialize(T, D)(D data) {
   static if (is(T == struct) || is(T == class)) {
-    return deserializeClass!T(data, conv);
+    return deserializeAggregate!T(data);
   }
-  else static if (isArray!T && !isSomeString!T) {
-    return deserializeArray!T(data, conv);
+  else static if (isInputRange!T && !isSomeString!T) {
+    return deserializeRange!T(data);
   }
   else {
-    return deserializeBasic!T(data, conv);
+    return deserializePrimitive!T(data);
   }
+}
+
+T deserializeEntry(T, D)(D data, string key) {
+  static assert(is(typeof(data.getEntry(key))),
+      "%s does not support aggregate deserialization (no getEntry function)".format(D.stringof));
+
+  auto entry = data.getEntry(key);
+  return deserialize!T(entry);
 }
 
 private:
-T deserializeClass(T, Data, Conv)(Data data, Conv conv) {
-  static assert(is(typeof(T.deserialize(data, conv)) : T),
-      T.stringof ~ " does not support deserialization. Did you mixin SerializeEnable?");
+T deserializeAggregate(T, D)(D data) {
+  static assert(hasMember!(T, "_deserialize"),
+      "%s does not support deserialization. Did you mixin SerializeEnable?".format(T.stringof));
 
-  return T.deserialize(data, conv);
+  return T._deserialize(data);
 }
 
-alias deserializeStruct = deserializeClass;
+T deserializeRange(T, D)(D data) {
+  static assert(isInputRange!(typeof(data.asRange())),
+      "%s must define 'asRange' to deserialize %s".format(D.stringof, T.stringof));
 
-T deserializeArray(T, Data, Conv)(Data data, Conv conv) {
-  static assert(is(typeof(conv.iterate(data))),
-      Conv.stringof ~ " must define 'iterate' to support desserializing arrays");
-
-  return conv.iterate(data).map!(x => deserialize!T(data, conv)).array;
+  return data.asRange.map!(x => deserialize!T(x)).array;
 }
 
-T deserializeBasic(T, Data, Conv)(Data data, Conv conv) {
-  static assert(is(typeof(conv.convert!T(data)) : T),
-      "%s does not know how to convert %s to %s".format(Conv.stringof, Data.stringof, T.stringof));
+T deserializePrimitive(T, D)(D data) {
+  static assert(is(typeof(data.asPrimitive!T()) : T),
+      "no known conversion %s.asPrimitive!%s".format(D.stringof, T.stringof));
 
-  return conv.convert!T(data);
+  return data.asPrimitive!T;
+}
+
+unittest {
+  import std.json;
+
+  static struct JsonWrapper {
+    private JSONValue _json;
+
+    auto asPrimitive(T : long)() { return cast(T) _json.integer; }
+    auto asRange()               { return _json.array.map!(x => JsonWrapper(x)); }
+    auto getEntry(string key)    { return JsonWrapper(_json.object[key]); }
+  }
+
+  static struct S {
+    int a, b;
+    static S _deserialize(T)(T data) {
+      return S(data.deserializeEntry!int("a"), data.deserializeEntry!int("b"));
+    }
+  }
+
+  auto str = q{[
+    { "a": 5, "b": 3 },
+    { "a": 1, "b": 2 }
+  ]};
+
+  auto json = JsonWrapper(str.parseJSON);
+  auto s = deserialize!(S[])(json);
+
+  assert(s[0] == S(5, 3));
+  assert(s[1] == S(1, 2));
 }
